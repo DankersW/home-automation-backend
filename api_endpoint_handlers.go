6610ae50430627b315f6bb526ad13110bf51e94c
DankersW <wouter.dankers@skynet.be>
2021-03-09 16:13:56 +0100

    Connecting to mongo db

--- api_endpoint_handlers.go
+++ api_endpoint_handlers.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"log"
 	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func handler_get_all_apis(context *gin.Context) {
 	apis := map[string]string{
-		"all":    "/api/",
-		"random": "/api/random",
+		"all":                     "/api/",
+		"random":                  "/api/random",
+		"iot_db_collection_names": "/api/iotDbCollectionNames",
 	}
 	context.JSON(http.StatusOK, gin.H{
 		"code":    http.StatusOK,
@@ -20,7 +23,24 @@ func handler_get_all_apis(context *gin.Context) {
 
 func handler_get_random(context *gin.Context) {
 	random_number := rand.Intn(50)
-	context.JSON(200, gin.H{
-		"Random": random_number,
+	context.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": random_number,
+	})
+}
+
+func handler_get_iot_db_collection_names(context *gin.Context) {
+	var code int
+	filter := bson.D{{}}
+	collection_names, err := iot_db.ListCollectionNames(db_ctx, filter)
+	if err != nil {
+		log.Fatal(err)
+		code = http.StatusBadGateway
+	} else {
+		code = http.StatusOK
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"code":    code,
+		"message": collection_names,
 	})
 }
--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/gin-gonic/gin"
+)
 
 var api_gateway *gin.Engine
+var iot_db *mongo.Database
+var db_ctx = context.TODO()
 
 func init() {
 	api_gateway = setup_api_gateway()
+	connect_to_mongo()
 }
 
 func main() {
@@ -22,4 +33,20 @@ func setup_api_gateway() *gin.Engine {
 func setup_api_endpoints(router_group *gin.RouterGroup) {
 	router_group.GET("/", handler_get_all_apis)
 	router_group.GET("/random", handler_get_random)
+	router_group.GET("/iotDbCollectionNames", handler_get_iot_db_collection_names)
+}
+
+func connect_to_mongo() {
+	clientOptions := options.Client().ApplyURI("mongodb://admin:mongo_admin_iot@192.168.1.140:27017/")
+	client, err := mongo.Connect(db_ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(db_ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	iot_db = client.Database("iot_db")
 }
