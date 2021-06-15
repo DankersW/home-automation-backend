254c7e98304780c8dd80cb3d3cfaf752aec92c60
DankersW <wouter.dankers@skynet.be>
2021-06-15 17:10:33 +0200

    db: temp call updated wih new interface

--- src/mongo_driver.go
+++ src/mongo_driver.go
@@ -41,20 +41,6 @@ func mongo_read(collection string, filter primitive.D) *mongo.Cursor {
 	return data_cursor
 }
 
-func read_device_sensor_data_collection() []SensorData {
-	// todo: make me generic
-	sensor_data_collection := iot_db.Collection("device_sensor_data")
-
-	filter := generate_timestamp_filter(7, 0)
-	data_cursor, err := sensor_data_collection.Find(db_ctx, filter)
-	if err != nil {
-		log.Fatal(err)
-	}
-	sensor_data := parse_sensor_data_document(data_cursor)
-	data_cursor.Close(context.TODO())
-	return sensor_data
-}
-
 func generate_timestamp_filter(oldest_day_limit int, max_day_limit int) primitive.D {
 	filter := bson.D{
 		primitive.E{
@@ -65,23 +51,4 @@ func generate_timestamp_filter(oldest_day_limit int, max_day_limit int) primitiv
 				Key: "$lte", Value: primitive.NewDateTimeFromTime(time.Now().AddDate(0, 0, -max_day_limit))}}},
 	}
 	return filter
-}
-
-func parse_sensor_data_document(cursor *mongo.Cursor) []SensorData {
-	sensor_data := []SensorData{}
-	for cursor.Next(db_ctx) {
-		var document_item bson.M
-		err := cursor.Decode(&document_item)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var sensor_item SensorData
-		sensor_item.Device_id = document_item["device_id"].(string)
-		sensor_item.Timestamp = document_item["timestamp"].(primitive.DateTime)
-		sensor_item.Temp = cast_to_float32(document_item["temperature"])
-		sensor_item.Humi = cast_to_float32(document_item["humidity"])
-		sensor_data = append(sensor_data, sensor_item)
-	}
-	return sensor_data
 }
